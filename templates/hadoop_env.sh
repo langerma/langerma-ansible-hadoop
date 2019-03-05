419346247122d52b6f50e182b730dc15ba25cc33
Markus Langer <langer.markus@gmail.com>
2019-03-05 12:51:58 +0100

    corrected hadoop env

--- templates/hadoop_env.sh
+++ templates/hadoop_env.sh
@@ -1,4 +1,5 @@
-export HADOOP_HOME={{hadoop_path}}/hadoop-{{hadoop_version}}
+export JAVA_HOME={{ java_install_dir}}/{{ java_version }}
+export HADOOP_HOME={{ hadoop_install_dir }}
 export PATH=$HADOOP_HOME/sbin:$HADOOP_HOME/bin:$PATH
 export HADOOP_LOG_DIR={{ hadoop_log_path }}
 export YARN_LOG_DIR=$HADOOP_LOG_DIR
